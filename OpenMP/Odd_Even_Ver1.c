5935fee944b3b63169de3e6d374ec5e1269c1d89
valdiviasharon <56985315+valdiviasharon@users.noreply.github.com>
2023-09-30 21:06:41 -0500

    Create Odd_Even_Ver1.c

---
+++ OpenMP/Odd_Even_Ver1.c
@@ -0,0 +1,102 @@
+/* 
+* Compile: gcc -g -Wall -fopenmp -o omp_odd_even1 omp_odd_even1.c
+* Usage:   ./omp_odd_even1 <thread count> <n> <g|i>
+*             n:   number of elements in list
+*            'g':  generate list using a random number generator
+            'i':  user input list
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#ifdef DEBUG
+const int RMAX = 100;
+#else
+const int RMAX = 10000000;
+#endif
+
+int thread_count;
+
+void Get_args(int argc, char* argv[], int* n_p) 
+{
+   thread_count = strtol(argv[1], NULL, 10);
+   *n_p = strtol(argv[2], NULL, 10);
+} 
+void Generate_list(int a[], int n) 
+{
+   srandom(1);
+   for (int i = 0; i < n; i++)
+      a[i] = random() % RMAX;
+} 
+void Print_list(int a[], int n, char* title) 
+{
+   int i;
+
+   printf("%s:\n", title);
+   for (i = 0; i < n; i++)
+      printf("%d ", a[i]);
+   printf("\n\n");
+}  
+
+void Odd_even(int a[], int n) {
+   int phase, i, tmp;
+#  ifdef DEBUG
+   char title[100];
+#  endif
+
+   for (phase = 0; phase < n; phase++) {
+      if (phase % 2 == 0)
+#        pragma omp parallel for num_threads(thread_count) \
+            default(none) shared(a, n) private(i, tmp)
+         for (i = 1; i < n; i += 2) {
+            if (a[i-1] > a[i]) {
+               tmp = a[i-1];
+               a[i-1] = a[i];
+               a[i] = tmp;
+            }
+         }
+      else
+#        pragma omp parallel for num_threads(thread_count) \
+            default(none) shared(a, n) private(i, tmp)
+         for (i = 1; i < n-1; i += 2) {
+            if (a[i] > a[i+1]) {
+               tmp = a[i+1];
+               a[i+1] = a[i];
+               a[i] = tmp;
+            }
+         }
+#     ifdef DEBUG
+      sprintf(title, "After phase %d", phase);
+      Print_list(a, n, title);
+#     endif
+   }
+}
+
+int main(int argc, char* argv[]) 
+{
+   int  n;
+   int* a;
+   double start, finish;
+
+   Get_args(argc, argv, &n);
+   a = malloc(n*sizeof(int));
+
+   Generate_list(a, n);
+#  ifdef DEBUG
+   Print_list(a, n, "Before sort");
+#  endif
+
+   start = omp_get_wtime();
+   Odd_even(a, n);
+   finish = omp_get_wtime();
+
+#  ifdef DEBUG
+   Print_list(a, n, "After sort");
+#  endif
+   
+   printf("Elapsed time = %e seconds\n", finish - start);
+
+   free(a);
+   return 0;
+}
